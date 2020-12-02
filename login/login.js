48a3b8d8e151457649dabc751f881041a0c23981
ALDA <higherstep14@gmail.com>
2020-12-02 18:38:09 +0800

    add validation to login page

--- login/login.js
+++ login/login.js
@@ -1,4 +1,21 @@
-let usernameIsInvalid = false;
-let passwordIsInvalid = false;
 
-let username = document.querySelector('#username')
+document.querySelector('#login').addEventListener('click', function(){
+    let emailIsInvalid = false;
+    let passwordIsInvalid = false;
+    
+    let email = document.querySelector('#email').value 
+    if (email.includes('@')==false||email.includes('.com')==false){
+        emailIsInvalid = true;   
+    } 
+
+    let password = document.querySelector('#password').value
+    if (password.length > 12 || password.length < 8){
+        passwordIsInvalid= true;
+    }
+
+    if (emailIsInvalid || passwordIsInvalid){
+        let alertEmail = document.querySelector('#emailInvalid')
+        alertEmail.innerHTML="*Email or password are invalid"; 
+    }
+
+})

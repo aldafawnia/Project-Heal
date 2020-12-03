cf5efce2a60923be8374807517c1aa13f1ea1057
ALDA <higherstep14@gmail.com>
2020-12-03 17:22:35 +0800

    add location market on maps

--- contact_us/contact.js
+++ contact_us/contact.js
@@ -1,5 +1,5 @@
 let camdenSingapore = [1.3032,103.8239];
-let map= L.map('map').setView(camdenSingapore,13);
+let map= L.map('map').setView(camdenSingapore,14);
 
 // setup tile layers
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,4 +11,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' 
 }).addTo(map);
 
+// add market 
+let camdenMarker = L.marker([1.3032,103.8239]);
+camdenMarker.addTo(map);
+camdenMarker.bindPopup(`
+<div>
+<img src="../images/camden-logo.png" height=110px width=230px>
+<hr>
+<p class="clinic">#14-02, Heal Therapy Clinic<p>
+</div>`);
 
